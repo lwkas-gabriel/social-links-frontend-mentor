d4698123ac7b531c8989a4fed4b9224861700b26
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-23 11:16:53 -0300

    bug fix 2 on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Button } from "./components/Button";
 import { operations } from "./constants/operationConstants";
 import image from "./assets/avatar-jessica.jpeg";
 
-import './App.module.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
-    <main>
-      <img src={image} />
-        <h1>Jessica Randall</h1>
-        <p>London, Unite Kingdom</p>
-        <span>&#34;Front-end developer avid reader&#34;</span>
+    <main className={styles.container}>
+      <img className={styles.avatar} src={image} />
+        <h1 className={styles.name}>Jessica Randall</h1>
+        <p className={styles.description}>London, Unite Kingdom</p>
+        <span className={styles.habits}>&#34;Front-end developer avid reader&#34;</span>
         
-        <section>
+        <section  className={styles.buttonContainer}>
           {operations.map((op) => {
               return (<Button key={op.id} text={op.text} link={op.link}/>)
           })}
